905fe04074a8f6f572c6684b08dbc8c0f94ed5d9
Alex Wilhelm <awwilhelm2014@gmail.com>
2015-08-03 13:23:00 -0500

    Fixed everything to be functional again

--- Assets/Scripts/GenerateWorld.cs
+++ Assets/Scripts/GenerateWorld.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class GenerateWorld : MonoBehaviour {
+public class GenerateWorld : MonoBehaviour
+{
 
 	public GameObject player;
 	public GameObject flatTerrain;
@@ -19,7 +20,7 @@ public class GenerateWorld : MonoBehaviour {
 	private ManageWorld manageWorldScript;
 
 	private GameObject generatedTerrain;
-	private ArrayList terrainType = new ArrayList();
+	private ArrayList terrainType = new ArrayList ();
 	private Queue<GameObject> terrain;
 	private Vector2 nextBlockLocation;
 	private Vector2 leftBlockLocation;
@@ -28,7 +29,7 @@ public class GenerateWorld : MonoBehaviour {
 	private int countTerrainBlocks;
 	private int currentNumberOfBlocks;
 
-	private delegate void AddCorrectDirection(Path path);
+	private delegate void AddCorrectDirection (Path path);
 	private AddCorrectDirection addCorrectDirection;
 	private bool choosingPath = false;
 
@@ -40,373 +41,322 @@ public class GenerateWorld : MonoBehaviour {
 		current,
 		right,
 		left
-	};
+	}
+	;
 
 	private enum Theme
 	{
 		normal,
 		other
-	};
-
-	void Awake()
+	}
+	;
+	
+	//Adds Generated terrain Gameobject to hold all of the terrain
+	void Awake ()
 	{
-		generatedTerrain = new GameObject("Generated Terrain");
-		generatedTerrain.AddComponent<ManageWorld>();
+		generatedTerrain = new GameObject ("Generated Terrain");
+		generatedTerrain.AddComponent<ManageWorld> ();
 
 		generatedTerrain.transform.position = Vector3.zero;
 	}
 
+	//Sets everything up
 	void Start ()
 	{
-		manageWorldScript = generatedTerrain.GetComponent<ManageWorld>();
-		manageWorldScript.SetForward();
-		terrain = new Queue<GameObject>();
+		manageWorldScript = generatedTerrain.GetComponent<ManageWorld> ();
+		manageWorldScript.SetForward ();
+		terrain = new Queue<GameObject> ();
 		addCorrectDirection = AddZPosition;
-		nextBlockLocation = new Vector2(0, -4);
+		nextBlockLocation = new Vector2 (0, -4);
 		leftBlockLocation = Vector2.zero;
 		rightBlockLocation = Vector2.zero;
 		terrainRotation = 0;
 		countTerrainBlocks = 0;
 		currentNumberOfBlocks = 0;
-		GenerateStartingTerrain();
-		PlaceStartingTerrain();
+		GenerateStartingTerrain ();
+		PlaceStartingTerrain ();
 	}
 
-	private void GenerateStartingTerrain()
+	//Generates terrain until it meets the NUMBER_OF_BLOCKS requriement
+	private void GenerateStartingTerrain ()
 	{
-		for(int i = 0; i< MIN_RUN_SPACE; i++)
-		{
-			terrainType.Add(1);
+		for (int i = 0; i< MIN_RUN_SPACE; i++) {
+			terrainType.Add (1);
 		}
-		for(int i = MIN_RUN_SPACE; i<NUMBER_OF_BLOCKS; i++)
-		{
-			terrainType.Add(GenerateRandomType());
+		for (int i = MIN_RUN_SPACE; i<NUMBER_OF_BLOCKS; i++) {
+			terrainType.Add (GenerateRandomType ());
 		}
 	}
 
-	private void PlaceStartingTerrain()
+	//Places the starting terrain
+	private void PlaceStartingTerrain ()
 	{
-		for(int i = 0; i<terrainType.Count; i++)
-		{
-			if(!choosingPath)
-			{
-				PickBlock((int)terrainType[i]);
+		for (int i = 0; i<terrainType.Count; i++) {
+			if (!choosingPath) {
+				PickBlock ((int)terrainType [i]);
 			}
 		}
 	}
 
-	private int GenerateRandomType()
+	//Picks a random number (some terrain has different probabilities of being picked)
+	private int GenerateRandomType ()
 	{
-		if(Random.Range(0,10) < 1)
+		if (Random.Range (0, 10) < 1)
 			return 1;
-		else if(Random.Range(0,30) < 1)
+		else if (Random.Range (0, 30) < 1)
 			return 10;
-		return Random.Range(1,10);
+		return Random.Range (1, 10);
 	}
 
-	private void PickBlock(int type)
+	//Defines what each random number will add
+	private void PickBlock (int type)
 	{
-		if(type == 1)
-		{
-			AddFlatTerrain(Theme.normal);
-		}
-		else if(type == 2)
-		{
-			AddJumpObstacleTerrain(Theme.normal);
-		}
-		else if(type == 3)
-		{
-			AddJumpGapTerrain(Theme.normal);
-		}
-		else if(type == 4)
-		{
-			AddJumpLeftGapTerrain(Theme.normal);
-		}
-		else if(type == 5)
-		{
-			AddJumpCenterGapTerrain(Theme.normal);
-		}
-		else if(type == 6)
-		{
-			AddJumpRightGapTerrain(Theme.normal);
-		}
-		else if(type == 7)
-		{
-			AddDuckObstacleTerrain(Theme.normal);
-		}
-		else if(type == 8) //Right turn
-		{
-			AddMakeRightTurn(Theme.normal);
-		}
-		else if(type == 9) //Left turn
-		{
-			AddMakeLeftTurn(Theme.normal);
-		}
-		else if(type == 10)
-		{
-			AddTPath(Theme.normal);
+		if (type == 1) {
+			AddFlatTerrain (Theme.normal);
+		} else if (type == 2) {
+			AddJumpObstacleTerrain (Theme.normal);
+		} else if (type == 3) {
+			AddJumpGapTerrain (Theme.normal);
+		} else if (type == 4) {
+			AddJumpLeftGapTerrain (Theme.normal);
+		} else if (type == 5) {
+			AddJumpCenterGapTerrain (Theme.normal);
+		} else if (type == 6) {
+			AddJumpRightGapTerrain (Theme.normal);
+		} else if (type == 7) {
+			AddDuckObstacleTerrain (Theme.normal);
+		} else if (type == 8) { //Right turn
+			AddMakeRightTurn (Theme.normal);
+		} else if (type == 9) { //Left turn
+			AddMakeLeftTurn (Theme.normal);
+		} else if (type == 10) {
+			AddTPath (Theme.normal);
 		}
 	}
 
-	private void AddBlock(GameObject init, Path path)
+	//Adds the block with the correct rotation, position, and other ways of tracking what block you are on
+	private void AddBlock (GameObject init, Path path)
 	{
 		Vector2 tempNextLocation;
-		if(path == Path.current)
-		{
+		if (path == Path.current) {
 			tempNextLocation = nextBlockLocation;
-		}
-		else if(path == Path.left)
-		{
+		} else if (path == Path.left) {
 			tempNextLocation = leftBlockLocation;
-		}
-		else if(path == Path.right)
-		{
+		} else if (path == Path.right) {
 			tempNextLocation = rightBlockLocation;
-		}
-		else
-		{
+		} else {
 			print ("ERROR: Path was not chosen");
 			return;
 		}
 
-		init.transform.rotation = Quaternion.Euler(0, terrainRotation, 0);
+		init.transform.rotation = Quaternion.Euler (0, terrainRotation, 0);
 
-		changeTerrainRotation();
+		changeTerrainRotation ();
 
 		init.transform.parent = generatedTerrain.transform;
-		init.transform.localPosition = new Vector3(tempNextLocation.x*10, -0.5f, tempNextLocation.y*10);
-		init.name = name+countTerrainBlocks;
+		init.transform.localPosition = new Vector3 (tempNextLocation.x * 10, -0.5f, tempNextLocation.y * 10);
+		init.name = name + countTerrainBlocks;
 		countTerrainBlocks++;
 		currentNumberOfBlocks++;
-		addCorrectDirection(path);
-		terrain.Enqueue(init);
+		addCorrectDirection (path);
+		terrain.Enqueue (init);
 	}
 
-	public void RemoveBlock()
+	//Removes oldest block
+	public void RemoveBlock ()
 	{
-		if(terrain.Count > 0)
-		{
-			GameObject remove = terrain.Dequeue();
-			Destroy(remove);
+		if (terrain.Count > 0) {
+			GameObject remove = terrain.Dequeue ();
+			Destroy (remove);
 			currentNumberOfBlocks--;
-		}
-		else
-		{
+		} else {
 			print ("ERROR: could not remove block");
 		}
-		if(!choosingPath && currentNumberOfBlocks <= NUMBER_OF_BLOCKS)
-		{
-			PickBlock(GenerateRandomType());
+		if (!choosingPath && currentNumberOfBlocks <= NUMBER_OF_BLOCKS) {
+			PickBlock (GenerateRandomType ());
 		}
 	}
 
-	public void PlayerHitTurn(Path path)
+	//Determines which path the player chose when hitting the T Turn
+	public void PlayerChoseTTurn (Path path)
 	{
 		choosingPath = false;
-		if(path == Path.left)
-		{
+		if (path == Path.left) {
 			nextBlockLocation = leftBlockLocation;
-		}
-		else if(path == Path.right)
-		{
+		} else if (path == Path.right) {
 			nextBlockLocation = rightBlockLocation;
 		}
-		BringTerrainUpToDate();
+		BringTerrainUpToDate ();
 	}
 
-	private void BringTerrainUpToDate()
+	//After hitting T Path, regenerates world to have correct NUMBER_OF_BLOCKS
+	private void BringTerrainUpToDate ()
 	{
-		while(currentNumberOfBlocks < NUMBER_OF_BLOCKS)
-		{
-			PickBlock(GenerateRandomType());
+		while (currentNumberOfBlocks < NUMBER_OF_BLOCKS) {
+			PickBlock (GenerateRandomType ());
 		}
 	}
 
-	void changeTerrainRotation()
+	//Sets terrain rotation
+	void changeTerrainRotation ()
 	{
-		if(addCorrectDirection == AddZPosition)
+		if (addCorrectDirection == AddZPosition)
 			terrainRotation = 0;
-		else if(addCorrectDirection == AddXPosition)
+		else if (addCorrectDirection == AddXPosition)
 			terrainRotation = 90;
 		else
 			terrainRotation = -90;
 	}
 
-	private void AddZPosition(Path path)
+	//Adds counter to Z position
+	private void AddZPosition (Path path)
 	{
-		if(path == Path.current)
-		{
+		if (path == Path.current) {
 			nextBlockLocation.y++;
-		}
-		else if(path == Path.left)
-		{
+		} else if (path == Path.left) {
 			leftBlockLocation.y++;
-		}
-		else if(path == Path.right)
-		{
+		} else if (path == Path.right) {
 			rightBlockLocation.y++;
 		}
 	}
 
-	private void AddXPosition(Path path)
+	//Adds counter to X position (to the right)
+	private void AddXPosition (Path path)
 	{
-		if(path == Path.current)
-		{
+		if (path == Path.current) {
 			nextBlockLocation.x++;
-		}
-		else if(path == Path.left)
-		{
+		} else if (path == Path.left) {
 			leftBlockLocation.x++;
-		}
-		else if(path == Path.right)
-		{
+		} else if (path == Path.right) {
 			rightBlockLocation.x++;
 		}
 	}
 
-	private void MinusXPosition(Path path)
+	//Minus counter to X position (to the left)
+	private void MinusXPosition (Path path)
 	{
-		if(path == Path.current)
-		{
+		if (path == Path.current) {
 			nextBlockLocation.x--;
-		}
-		else if(path == Path.left)
-		{
+		} else if (path == Path.left) {
 			leftBlockLocation.x--;
-		}
-		else if(path == Path.right)
-		{
+		} else if (path == Path.right) {
 			rightBlockLocation.x--;
 		}
 	}
 
-	private GameObject MakeLeftTurn(AddCorrectDirection XPos)
+	//Generates left turn
+	private GameObject MakeLeftTurn (AddCorrectDirection XPos)
 	{
 		addCorrectDirection = XPos;
-		return Instantiate(turnLeftTerrain, Vector3.zero, Quaternion.identity) as GameObject;
+		return Instantiate (turnLeftTerrain, Vector3.zero, Quaternion.identity) as GameObject;
 	}
 
-	private GameObject MakeRightTurn(AddCorrectDirection ZPos)
+	//Generates right turn
+	private GameObject MakeRightTurn (AddCorrectDirection ZPos)
 	{
 		addCorrectDirection = ZPos;
-		return Instantiate(turnRightTerrain, Vector3.zero, Quaternion.identity) as GameObject;
+		return Instantiate (turnRightTerrain, Vector3.zero, Quaternion.identity) as GameObject;
 	}
 
-	private void AddFlatTerrain(Theme theme)
+	private void AddFlatTerrain (Theme theme)
 	{
-		AddBlock(Instantiate(flatTerrain, Vector3.zero, Quaternion.identity) as GameObject, Path.current);
+		AddBlock (Instantiate (flatTerrain, Vector3.zero, Quaternion.identity) as GameObject, Path.current);
 	}
 
-	private void AddJumpObstacleTerrain(Theme theme)
+	private void AddJumpObstacleTerrain (Theme theme)
 	{
-		AddBlock(Instantiate(jumpObstacleTerrain, Vector3.zero, Quaternion.identity) as GameObject, Path.current);
+		AddBlock (Instantiate (jumpObstacleTerrain, Vector3.zero, Quaternion.identity) as GameObject, Path.current);
 	}
 
-	private void AddJumpGapTerrain(Theme theme)
+	private void AddJumpGapTerrain (Theme theme)
 	{
-		AddBlock(Instantiate(jumpGapTerrain, Vector3.zero, Quaternion.identity) as GameObject, Path.current);
+		AddBlock (Instantiate (jumpGapTerrain, Vector3.zero, Quaternion.identity) as GameObject, Path.current);
 	}
 
-	private void AddJumpLeftGapTerrain(Theme theme)
+	private void AddJumpLeftGapTerrain (Theme theme)
 	{
-		AddBlock(Instantiate(jumpLeftGapTerrain, Vector3.zero, Quaternion.identity) as GameObject, Path.current);
+		AddBlock (Instantiate (jumpLeftGapTerrain, Vector3.zero, Quaternion.identity) as GameObject, Path.current);
 	}
 
-	private void AddJumpCenterGapTerrain(Theme theme)
+	private void AddJumpCenterGapTerrain (Theme theme)
 	{
-		AddBlock(Instantiate(jumpCenterGapTerrain, Vector3.zero, Quaternion.identity) as GameObject, Path.current);
+		AddBlock (Instantiate (jumpCenterGapTerrain, Vector3.zero, Quaternion.identity) as GameObject, Path.current);
 	}
 
-	private void AddJumpRightGapTerrain(Theme theme)
+	private void AddJumpRightGapTerrain (Theme theme)
 	{
-		AddBlock(Instantiate(jumpRightGapTerrain, Vector3.zero, Quaternion.identity) as GameObject, Path.current);
+		AddBlock (Instantiate (jumpRightGapTerrain, Vector3.zero, Quaternion.identity) as GameObject, Path.current);
 	}
 
-	private void AddDuckObstacleTerrain(Theme theme)
+	private void AddDuckObstacleTerrain (Theme theme)
 	{
-		AddBlock(Instantiate(duckObstacleTerrain, Vector3.zero, Quaternion.identity) as GameObject, Path.current);
+		AddBlock (Instantiate (duckObstacleTerrain, Vector3.zero, Quaternion.identity) as GameObject, Path.current);
 	}
 
-	private void AddMakeRightTurn(Theme theme)
+	private void AddMakeRightTurn (Theme theme)
 	{
-		if(addCorrectDirection == MinusXPosition)
-		{
-			AddBlock(MakeRightTurn(AddZPosition), Path.current);
-		}
-		else if(addCorrectDirection == AddXPosition)
-		{
-			AddBlock(MakeLeftTurn(AddZPosition), Path.current);
-		}
-		else
-		{
-			AddBlock(MakeRightTurn(AddXPosition), Path.current);
+		if (addCorrectDirection == MinusXPosition) {
+			AddBlock (MakeRightTurn (AddZPosition), Path.current);
+		} else if (addCorrectDirection == AddXPosition) {
+			AddBlock (MakeLeftTurn (AddZPosition), Path.current);
+		} else {
+			AddBlock (MakeRightTurn (AddXPosition), Path.current);
 		}
 	}
 
-	private void AddMakeLeftTurn(Theme theme)
+	private void AddMakeLeftTurn (Theme theme)
 	{
-		if(addCorrectDirection == MinusXPosition)
-		{
-			AddBlock(MakeRightTurn(AddZPosition), Path.current);
-		}
-		else if(addCorrectDirection == AddXPosition)
-		{
-			AddBlock(MakeLeftTurn(AddZPosition), Path.current);
-		}
-		else
-		{
-			AddBlock(MakeLeftTurn(MinusXPosition), Path.current);
+		if (addCorrectDirection == MinusXPosition) {
+			AddBlock (MakeRightTurn (AddZPosition), Path.current);
+		} else if (addCorrectDirection == AddXPosition) {
+			AddBlock (MakeLeftTurn (AddZPosition), Path.current);
+		} else {
+			AddBlock (MakeLeftTurn (MinusXPosition), Path.current);
 		}
 	}
 
-	private void AddTPath(Theme theme)
+	private void AddTPath (Theme theme)
 	{
 		choosingPath = true;
-		if(addCorrectDirection == MinusXPosition)
-		{
-			AddBlock(MakeRightTurn(AddZPosition), Path.current);
-		}
-		else if(addCorrectDirection == AddXPosition)
-		{
-			AddBlock(MakeLeftTurn(AddZPosition), Path.current);
+		if (addCorrectDirection == MinusXPosition) {
+			AddBlock (MakeRightTurn (AddZPosition), Path.current);
+		} else if (addCorrectDirection == AddXPosition) {
+			AddBlock (MakeLeftTurn (AddZPosition), Path.current);
 		}
 		
-		for(int i = 0; i<4; i++)
-		{
-			AddBlock(Instantiate(flatTerrain, Vector3.zero, Quaternion.identity) as GameObject, Path.current);
+		for (int i = 0; i<4; i++) {
+			AddBlock (Instantiate (flatTerrain, Vector3.zero, Quaternion.identity) as GameObject, Path.current);
 		}
 		
 		leftBlockLocation = nextBlockLocation;
 		rightBlockLocation = nextBlockLocation;
 		
-		AddBlock(Instantiate(turnFork, Vector3.zero, Quaternion.identity) as GameObject, Path.current);
+		AddBlock (Instantiate (turnFork, Vector3.zero, Quaternion.identity) as GameObject, Path.current);
 		//split
 		
 		//Left
 		addCorrectDirection = MinusXPosition;
-		addCorrectDirection(Path.left);
-		changeTerrainRotation();
+		addCorrectDirection (Path.left);
+		changeTerrainRotation ();
 		
-		for(int i = 0; i<3; i++)
-		{
-			AddBlock(Instantiate(flatTerrain, Vector3.zero, Quaternion.identity) as GameObject, Path.left);
+		for (int i = 0; i<3; i++) {
+			AddBlock (Instantiate (flatTerrain, Vector3.zero, Quaternion.identity) as GameObject, Path.left);
 		}
-		AddBlock(MakeRightTurn(AddZPosition), Path.left);
-		changeTerrainRotation();
-		AddBlock(Instantiate(flatTerrain, Vector3.zero, Quaternion.identity) as GameObject, Path.left);
+		AddBlock (MakeRightTurn (AddZPosition), Path.left);
+		changeTerrainRotation ();
+		AddBlock (Instantiate (flatTerrain, Vector3.zero, Quaternion.identity) as GameObject, Path.left);
 		
 		//Right
 		addCorrectDirection = AddXPosition;
-		addCorrectDirection(Path.right);
-		changeTerrainRotation();
+		addCorrectDirection (Path.right);
+		changeTerrainRotation ();
 		
-		for(int i = 0; i<3; i++)
-		{
-			AddBlock(Instantiate(flatTerrain, Vector3.zero, Quaternion.identity) as GameObject, Path.right);
+		for (int i = 0; i<3; i++) {
+			AddBlock (Instantiate (flatTerrain, Vector3.zero, Quaternion.identity) as GameObject, Path.right);
 		}
-		AddBlock(MakeLeftTurn(AddZPosition), Path.right);
-		changeTerrainRotation();
-		AddBlock(Instantiate(flatTerrain, Vector3.zero, Quaternion.identity) as GameObject, Path.right);
+		AddBlock (MakeLeftTurn (AddZPosition), Path.right);
+		changeTerrainRotation ();
+		AddBlock (Instantiate (flatTerrain, Vector3.zero, Quaternion.identity) as GameObject, Path.right);
 	}
 
 
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
 	public LayerMask playerMask;
 	private GameObject spawnPoint;
@@ -31,170 +32,153 @@ public class PlayerController : MonoBehaviour {
 	Vector3 currPosBasedOnState;
 	Vector3 temp;
 
-	void Start () {
-		spawnPoint = GameObject.Find("SpawnPoint");
-		generatedTerrain = GameObject.Find("Generated Terrain");
-		generateWorldScript = GameObject.Find("World").GetComponent<GenerateWorld>();
-		thirdPersonCameraScript = Camera.main.GetComponent<ThirdPersonCamera>();
-		manageWorldScript = generatedTerrain.GetComponent<ManageWorld>();
+	void Start ()
+	{
+		spawnPoint = GameObject.Find ("SpawnPoint");
+		generatedTerrain = GameObject.Find ("Generated Terrain");
+		generateWorldScript = GameObject.Find ("World").GetComponent<GenerateWorld> ();
+		thirdPersonCameraScript = Camera.main.GetComponent<ThirdPersonCamera> ();
+		manageWorldScript = generatedTerrain.GetComponent<ManageWorld> ();
 		collidedOnce = false;
 
-		Spawn();
+		Spawn ();
 	}
 
 	void Update ()
 	{
 		Vector3 futurePosBasedOnState;
-		if(Input.GetButton("Horizontal"))
-		{
-			transform.Translate(Vector3.right * Input.GetAxis("Horizontal")*Time.deltaTime*10);
+		if (Input.GetButton ("Horizontal")) {
+			transform.Translate (Vector3.right * Input.GetAxis ("Horizontal") * Time.deltaTime * 10);
 
-			if(transform.position.x >= RUNWAY_EDGE)
-			{
-				transform.position = new Vector3(RUNWAY_EDGE, transform.position.y, transform.position.z);
-			}
-			else if(transform.position.x <= -RUNWAY_EDGE)
-			{
-				transform.position = new Vector3(-RUNWAY_EDGE, transform.position.y, transform.position.z);
+			if (transform.position.x >= RUNWAY_EDGE) {
+				transform.position = new Vector3 (RUNWAY_EDGE, transform.position.y, transform.position.z);
+			} else if (transform.position.x <= -RUNWAY_EDGE) {
+				transform.position = new Vector3 (-RUNWAY_EDGE, transform.position.y, transform.position.z);
 			}
 			
-			if (transform.position.z >= RUNWAY_EDGE)
-			{
-				transform.position = new Vector3(transform.position.x , transform.position.y, RUNWAY_EDGE);
-			}
-			else if(transform.position.z <= -RUNWAY_EDGE)
-			{
-				transform.position = new Vector3(transform.position.x, transform.position.y, -RUNWAY_EDGE);
+			if (transform.position.z >= RUNWAY_EDGE) {
+				transform.position = new Vector3 (transform.position.x, transform.position.y, RUNWAY_EDGE);
+			} else if (transform.position.z <= -RUNWAY_EDGE) {
+				transform.position = new Vector3 (transform.position.x, transform.position.y, -RUNWAY_EDGE);
 			}
 		}
 
-		if(Input.GetButton("Jump") && !jumping && !ducking)
-		{
+		if (Input.GetButton ("Jump") && !jumping && !ducking) {
 			velocity = startingVelocity;
 			jumping = true;
 		}
 
-		if(Input.GetButton("Duck") && !ducking && !jumping)
-		{
+		if (Input.GetButton ("Duck") && !ducking && !jumping) {
 			ducking = true;
 			crouchVal = 0.5f;
 			timePressedDuck = Time.time;
-			transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y-crouchVal, transform.localScale.z);
+			transform.localScale = new Vector3 (transform.localScale.x, transform.localScale.y - crouchVal, transform.localScale.z);
 		}
 
-		if(ducking && Time.time - timePressedDuck > duckingLength)
-		{
+		if (ducking && Time.time - timePressedDuck > duckingLength) {
 			ducking = false;
 			crouchVal = 1;
-			transform.position = new Vector3(transform.position.x, spawnPoint.transform.position.y + BUFFER, transform.position.z);
-			transform.localScale = new Vector3(1 ,1 ,1);
+			transform.position = new Vector3 (transform.position.x, spawnPoint.transform.position.y + BUFFER, transform.position.z);
+			transform.localScale = new Vector3 (1, 1, 1);
 		}
+
+		HandleGravity ();
 
 		collidedOnce = false;
 	}
 
-	public bool GetCrouch()
+	public bool GetCrouch ()
 	{
 		return ducking;
 	}
 
-	public bool GetJump()
+	public bool GetJump ()
 	{
 		return jumping;
 	}
 
-	private void HandleGravity()
+	private void HandleGravity ()
 	{
-		float futureVelocity = velocity + gravity*Time.deltaTime;
-		float futureLength = Time.deltaTime * (futureVelocity + velocity)/2;
-		futureLength = Mathf.Abs(futureLength);
+		float futureVelocity = velocity + gravity * Time.deltaTime;
+		float futureLength = Time.deltaTime * (futureVelocity + velocity) / 2;
+		futureLength = Mathf.Abs (futureLength);
 		float dir = velocity > 0 ? 1 : -1;
 		velocity = futureVelocity;
-		currPosBasedOnState = new Vector3(transform.position.x, transform.position.y - transform.localScale.y+SKIN_WIDTH, transform.position.z);
+		currPosBasedOnState = new Vector3 (transform.position.x, transform.position.y - transform.localScale.y + SKIN_WIDTH, transform.position.z);
 		
-		Physics.Raycast(currPosBasedOnState, Vector3.up * dir, out hit, futureLength+SKIN_WIDTH);
+		Physics.Raycast (currPosBasedOnState, Vector3.up * dir, out hit, futureLength + SKIN_WIDTH);
 		
-		if(dir < 0)
-		{
-			if(hit.transform && hit.transform.tag == "Ground")
-			{
+		if (dir < 0) {
+			if (hit.transform && hit.transform.tag == "Ground") {
 				//print ("hit  "+ (transform.position.y-transform.localScale.y));
-				transform.position = new Vector3(transform.position.x, hit.point.y + transform.localScale.y, transform.position.z);
+				transform.position = new Vector3 (transform.position.x, hit.point.y + transform.localScale.y, transform.position.z);
 				velocity = 0;
 				jumping = false;
+			} else {
+				transform.position = new Vector3 (transform.position.x, transform.position.y + (futureLength * dir), transform.position.z);
 			}
-			else
-			{
-				transform.position = new Vector3(transform.position.x, transform.position.y + (futureLength * dir), transform.position.z);
-			}
-		}
-		else
-		{
-			transform.position = new Vector3(transform.position.x, transform.position.y + futureLength, transform.position.z);
+		} else {
+			transform.position = new Vector3 (transform.position.x, transform.position.y + futureLength, transform.position.z);
 		}
 	}
 
-	void OnCollisionEnter (Collision col)
+	void OnTriggerEnter (Collider col)
 	{
-		if(col.transform.tag == "Death")
-		{
-			Application.LoadLevel(0);
-			//Spawn();
-			//generateWorldScript.StartNewGame();
+		print ("collision " + col.transform.name);
+		if (col.transform.tag == "Death") {
+			Application.LoadLevel (0);
+		}
+		if (!collidedOnce) {
+			if (col.transform.tag == "TurningRight") {
+				if (col.transform.parent.tag == "TurningFork") {
+					generateWorldScript.PlayerChoseTTurn (GenerateWorld.Path.right);
+				}
+
+				manageWorldScript.TurnRight ();
+				manageWorldScript.RoundTransformPosition (col);
+				transform.rotation = Quaternion.Euler (transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + 90, transform.rotation.eulerAngles.z);
+				if (manageWorldScript.GetDirection () == ManageWorld.Direction.forward) {
+					transform.position = new Vector3 (transform.position.z, transform.position.y, transform.position.z);
+				} else if (manageWorldScript.GetDirection () == ManageWorld.Direction.right) {
+					transform.position = new Vector3 (transform.position.x, transform.position.y, -(transform.position.x));
+				}
+				thirdPersonCameraScript.Turning ();
+				collidedOnce = true;
+				Destroy (col.transform.parent.gameObject);
+			} else if (col.transform.tag == "TurningLeft") {
+				if (col.transform.parent.tag == "TurningFork") {
+					generateWorldScript.PlayerChoseTTurn (GenerateWorld.Path.left);
+				}
+
+				manageWorldScript.TurnLeft ();
+				manageWorldScript.RoundTransformPosition (col);
+				transform.rotation = Quaternion.Euler (transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y - 90, transform.rotation.eulerAngles.z);
+				if (manageWorldScript.GetDirection () == ManageWorld.Direction.forward) {
+					transform.position = new Vector3 (-(transform.position.z), transform.position.y, transform.position.z);
+				} else if (manageWorldScript.GetDirection () == ManageWorld.Direction.left) {
+					transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.x);
+				}
+				thirdPersonCameraScript.Turning ();
+				collidedOnce = true;
+				Destroy (col.transform.parent.gameObject);
+			}
+
 		}
 	}
 
-	void OnTriggerEnter(Collider col)
+	void Spawn ()
 	{
-		if(!collidedOnce)
-		{
-			if(col.transform.tag == "TurningRight")
-			{
-				if(col.transform.parent.tag == "TurningFork")
-				{
-					generateWorldScript.PlayerHitTurn(GenerateWorld.Path.right);
-				}
-
-				manageWorldScript.TurnRight();
-				manageWorldScript.RoundTransformPosition(col);
-				transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + 90, transform.rotation.eulerAngles.z);
-				if(manageWorldScript.GetDirection() == ManageWorld.Direction.forward)
-				{
-					transform.position = new Vector3(transform.position.z, transform.position.y, transform.position.z);
-				}
-				else if(manageWorldScript.GetDirection() == ManageWorld.Direction.right)
-				{
-					transform.position = new Vector3(transform.position.x, transform.position.y, -(transform.position.x));
-				}
-				thirdPersonCameraScript.Turning();
-				collidedOnce = true;
-				Destroy(col.transform.parent.gameObject);
-			}
-
-			else if(col.transform.tag == "TurningLeft")
-			{
-				if(col.transform.parent.tag == "TurningFork")
-				{
-					generateWorldScript.PlayerHitTurn(GenerateWorld.Path.left);
-				}
-
-				manageWorldScript.TurnLeft();
-				manageWorldScript.RoundTransformPosition(col);
-				transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y - 90, transform.rotation.eulerAngles.z);
-				if(manageWorldScript.GetDirection() == ManageWorld.Direction.forward)
-				{
-					transform.position = new Vector3(-(transform.position.z), transform.position.y, transform.position.z);
-				}
-				else if(manageWorldScript.GetDirection() == ManageWorld.Direction.left)
-				{
-					transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.x);
-				}
-				thirdPersonCameraScript.Turning();
-				collidedOnce = true;
-				Destroy(col.transform.parent.gameObject);
-			}
-
+		if (spawnPoint != null) {
+			transform.position = new Vector3 (spawnPoint.transform.position.x, spawnPoint.transform.position.y + BUFFER, spawnPoint.transform.position.z);
 		}
-	}
 
+		thirdPersonCameraScript.Restart ();
+		generatedTerrain.transform.position = Vector3.zero;
+		transform.localRotation = Quaternion.Euler (transform.localRotation.eulerAngles.x, 0, transform.rotation.eulerAngles.z);
+		transform.localScale = new Vector3 (1, 1, 1);
+		jumping = false;
+		ducking = false;
+		velocity = 0;
+		crouchVal = 1;
+	}
 }
